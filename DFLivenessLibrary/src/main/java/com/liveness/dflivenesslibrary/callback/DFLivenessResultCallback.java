d17406fe7573cb7f05a345aed84ff8d342a1c961
user1 <492940328@qq.com>
2022-12-12 17:45:59 +0800

    feature:活体检测库1.0.7去除aar依赖

--- DFLivenessLibrary/src/main/java/com/liveness/dflivenesslibrary/callback/DFLivenessResultCallback.java
+++ DFLivenessLibrary/src/main/java/com/liveness/dflivenesslibrary/callback/DFLivenessResultCallback.java
@@ -4,7 +4,7 @@ package com.liveness.dflivenesslibrary.callback;
 import com.dfsdk.liveness.DFLivenessSDK;
 
 public interface DFLivenessResultCallback {
-//    void saveFinalEncrytFile(byte[] livenessEncryptResult, DFLivenessSDK.DFLivenessImageResult[] imageResult, byte[] videoResult);
+    void saveFinalEncrytFile(byte[] livenessEncryptResult, byte[] imageResult, byte[] videoResult);
 
     void saveFile(byte[] livenessEncryptResult);
 
